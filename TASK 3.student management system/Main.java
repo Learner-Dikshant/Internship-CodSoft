14f6fecfc3b52c09c69b4ca1bbe4974fc37cb31c
Dikshant kandpal <91550376+dikshant12356@users.noreply.github.com>
2023-07-26 18:04:29 +0530

    Add files via upload
    open file "Main.java" to execute the program

---
+++ TASK 3.student management system/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+    	studentmanagementsystem st = new studentmanagementsystem();
+    	st.pack();
+        st.setVisible(true);
+        st.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
